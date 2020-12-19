e7a5cbf3d90e1761e1eab03b87cf63904a9edf2c
william koller <wkoller25@gmail.com>
2020-12-19 00:52:34 -0300

    feat: add AuthService com valid token

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { compareSync } from 'bcrypt'
+import { User } from 'src/user/user.entity'
+import { UserService } from 'src/user/user.service'
+import { AuthInput } from './dto/auth.input'
+import { AuthType } from './dto/auth.type'
+import { JwtService } from '@nestjs/jwt'
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly userService: UserService, private readonly jwtService: JwtService) {}
+
+  async validateUser(data: AuthInput): Promise<AuthType> {
+    const user = await this.userService.findUserByEmail(data.email)
+
+    const validPassword = compareSync(data.password, user.password)
+
+    if (!validPassword) {
+      throw new UnauthorizedException('Incorrect password.')
+    }
+
+    const token = await this.jwtToken(user)
+
+    return {
+      user,
+      token,
+    }
+  }
+
+  private async jwtToken(user: User): Promise<string> {
+    const payload = {
+      username: user.name,
+      sub: user.id,
+    }
+
+    return this.jwtService.signAsync(payload)
+  }
+}
