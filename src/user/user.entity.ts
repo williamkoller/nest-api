a1d937de2b83e7d1a075aa15e2f4b1c7df7589ac
william koller <wkoller25@gmail.com>
2020-12-18 23:00:26 -0300

    refactor: add field password call hashPasswordTransform

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,4 +1,5 @@
-import { Field, ID, ObjectType } from '@nestjs/graphql'
+import { Field, HideField, ID, ObjectType } from '@nestjs/graphql'
+import { hashPasswordTransform } from 'src/commom/helpers/crypto'
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
 @ObjectType()
@@ -13,4 +14,10 @@ export class User {
 
   @Column()
   email: string
+
+  @Column({
+    transformer: hashPasswordTransform,
+  })
+  @HideField()
+  password: string
 }
