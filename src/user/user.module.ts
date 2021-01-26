2eff28364f55c94dd52b07aa7e9943cbbc44e351
william koller <wkoller25@gmail.com>
2021-01-25 21:08:47 -0300

    refacotr: move file user entity

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common'
-import { UserService } from './user.service'
-import { UserResolver } from './user.resolver'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { User } from './user.entity'
+import { Module } from '@nestjs/common';
+import { UserService } from './services/user.service';
+import { UserResolver } from './resolvers/user.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
